4d4faaf4b06f09e902a10da35620dc26b0bd311f
tony-xu1 <tonyxu1257@gmail.com>
2026-09-16 14:31:10 -0700

    Add files via upload

---
+++ Temperature.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Temperature {
+	
+	public static void main (String[] args){
+	// scanner
+	Scanner in = new Scanner(System.in);
+	
+	//final constants
+	final double C_PER_F = 9.0/5.0;
+	final int OFFSET = 18;
+	
+	System.out.print("Enter a temperature in Celsius: ");
+	double celsius = in.nextDouble();
+			in.nextLine();
+		
+		double farenheit = (celsius * C_PER_F) + OFFSET;
+	System.out.printf("Temperature in Farenheit: %.2f F", farenheit);
+	
+	}
+}
