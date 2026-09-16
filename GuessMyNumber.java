2c4e2c36b78af15862c1aaedbf6eba63cdc62ca6
tony-xu1 <tonyxu1257@gmail.com>
2026-09-16 15:00:58 -0700

    Add files via upload

--- GuessMyNumber.java
+++ GuessMyNumber.java
@@ -2,7 +2,17 @@ import java.util.Scanner;
 import java.util.Random;
 
 pulic class GuessMyNumber {
+	public static void main (String[] args){
+	
+	Random random = new Random();
+	int number = random.nextInt(100) + 1;
+	
+	Scanner in = new Scanner(System.in);
+	int guess = in.nextInt();
+				nextLine();
+	
+	System.out.println("I'm thinking of a number from 1 to 100. Can you guess what it is?");
 
 
-
+	}
 }
